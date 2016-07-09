ec9c46976ca2b00de61b859c321636bf7b402ffa
Leos Literák <literakl@centrum.cz>
2016-07-09 10:39:11 +0200

    Initial commit

---
+++ app/src/main/java/com/lelisoft/sandbox/mpchart/StartActivity.java
@@ -0,0 +1,102 @@
+package com.lelisoft.sandbox.mpchart;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Leoš on 08.07.2016.
+ */
+public class StartActivity extends AppCompatActivity {
+    private LineChart mChart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        mChart = (LineChart) findViewById(R.id.chart);
+
+        mChart.setDrawGridBackground(false);
+
+        // no description text
+        mChart.setDescription("");
+        mChart.setNoDataTextDescription("You need to provide data for the chart.");
+
+        // enable touch gestures
+        mChart.setTouchEnabled(true);
+
+        // enable scaling and dragging
+        mChart.setDragEnabled(true);
+//        mChart.setScaleEnabled(true);
+         mChart.setScaleXEnabled(true);
+         mChart.setScaleYEnabled(false);
+
+        // if disabled, scaling can be done on x- and y-axis separately
+        mChart.setPinchZoom(true);
+
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+//        xAxis.setTypeface(mTfLight);
+        xAxis.setDrawGridLines(false);
+        xAxis.setGranularity(1f); // only intervals of 1 day
+        xAxis.setLabelCount(7);
+        xAxis.setValueFormatter(new DayAxisValueFormatter(mChart));
+
+        setData();
+        mChart.animateX(500);
+
+        // get the legend (only possible after setting data)
+        Legend l = mChart.getLegend();
+
+        // modify the legend ...
+        // l.setPosition(LegendPosition.LEFT_OF_CHART);
+        l.setForm(Legend.LegendForm.LINE);
+
+//        mChart.setVisibleXRangeMaximum(10);
+//        mChart.setVisibleXRangeMinimum(5);
+        mChart.moveViewToX(20);
+    }
+
+    private void setData() {
+        ArrayList<Entry> values = new ArrayList<Entry>();
+        for (int i = 0; i < 120; i++) {
+            float val = (float) (Math.random() * 6) + 3;
+            values.add(new Entry(i, val));
+        }
+
+        // create a dataset and give it a type
+        LineDataSet set1 = new LineDataSet(values, "DataSet 1");
+
+        // set the line to be drawn like this "- - - - - -"
+//        set1.enableDashedLine(10f, 5f, 0f);
+//        set1.enableDashedHighlightLine(10f, 5f, 0f);
+        set1.setColor(Color.BLACK);
+        set1.setCircleColor(Color.BLACK);
+        set1.setLineWidth(1f);
+        set1.setCircleRadius(3f);
+        set1.setDrawCircleHole(false);
+        set1.setValueTextSize(9f);
+        set1.setDrawFilled(true);
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
+        dataSets.add(set1); // add the datasets
+
+        // create a data object with the datasets
+        LineData data = new LineData(dataSets);
+
+        // set data
+        mChart.setData(data);
+    }
+}
